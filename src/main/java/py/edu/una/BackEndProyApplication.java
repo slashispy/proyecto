77f19be10396552b97aec57ad52e15ddaa95f823
slashispy <andresplanas91@gmail.com>
2019-07-03 16:01:45 -0400

    se modifico el proyecto para que pueda ser desplegable en wildfly

--- src/main/java/py/edu/una/BackEndProyApplication.java
+++ src/main/java/py/edu/una/BackEndProyApplication.java
@@ -2,6 +2,8 @@ package py.edu.una;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages={
@@ -10,9 +12,14 @@ import org.springframework.context.annotation.Import;
 		"py.edu.una.rest.dao"})
 @Import({JpaConfiguration.class, WebConfig.class})
 //@Import(JpaConfiguration.class)
-public class BackEndProyApplication {
+public class BackEndProyApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndProyApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BackEndProyApplication.class);
+    }
 }
